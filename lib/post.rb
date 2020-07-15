5787935ddced06f3f70102488f502a233d68af8b
irosen419 <irosen419@gmail.com>
2020-07-15 18:07:11 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,23 @@
+class Post
+    attr_accessor :title, :author
+
+    @@all = []
+
+    def initialize(title)
+        @title = title
+        save_post
+    end
+
+    def save_post
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def author_name
+        self.author ? self.author.name : nil
+    end
+
+end
