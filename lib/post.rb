ad72b6f5f194fe46f5ee9afaadcd53ca1a838a8e
amitdesh <amitdesh2014@gmail.com>
2020-07-16 17:28:15 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,31 @@
+class Post
+
+    attr_accessor :author
+
+    @@all = []
+
+    def initialize(title)
+        @title = title
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def title
+        @title
+    end
+
+    def author_name
+        if self.author == nil
+            nil
+        else
+            self.author.name
+        end
+    end
+
+
+
+
+end
