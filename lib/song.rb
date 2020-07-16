523784c3a15ef0b684208124b7b926681fa683ab
brianwlego <legobw@gmail.com>
2020-07-15 22:52:51 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+require_relative './artist.rb'
+require 'pry'
+
+class Song
+  attr_accessor :artist, :name
+  
+  @@all = []
+  def initialize(name)
+    @name = name
+    save
+  end
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    artist.name if artist
+  end
+  
+end
