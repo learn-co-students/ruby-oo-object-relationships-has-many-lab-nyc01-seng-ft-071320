5969e4d87e091aa47ee6a105151835ca8f01d515
victorscholz <vgscholz@gmail.com>
2020-07-16 17:07:16 -0400

    finished

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+require_relative './artist.rb'
+require 'pry'
+
+class Song
+    attr_accessor :name, :artist
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def artist_name
+        artist.name if artist
+    end
+end
