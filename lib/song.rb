8342b3688dfcf092b6cba0041a75fe08ba846495
kevin_xie <xie.kevin.j@gmail.com>
2020-07-18 16:25:03 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+require_relative './artist.rb'
+require 'pry'
+
+class Song 
+    attr_accessor :name, :artist
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+    
+    def self.all
+        @@all
+    end
+  
+    def artist_name
+        artist.name if artist 
+    end
+  
+    
+
+end
+
+# binding.pry  
