41846fd87a03dff0a15173b3bf1bb3bf7bffd6b2
dkim61 <dkim187@binghamton.edu>
2020-07-19 17:02:42 -0400

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,29 @@
+class Author
+    @@post_count = 0
+    attr_accessor :name
+    def initialize (name)
+        @name = name
+    end
+
+    def posts
+        Post.all.select do |post| 
+            post.author = self
+        end
+    end
+
+    def add_post(post)
+        post.author = self
+        @@post_count += 1
+    end
+
+    def add_post_by_title(title)
+        post = Post.new(title)
+        post.author = self
+        @@post_count += 1
+    end
+
+    def self.post_count
+        Post.all.count
+    end
+
+end
