9461b17a517f7a3cb56796a8f881c2155bf5953b
vnapuri <vladnapuri@gmail.com>
2020-07-19 17:37:35 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+require 'pry'
+require_relative './song.rb'
+
+class Artist
+    
+    attr_accessor :name, :songs
+
+
+    
+    def initialize(name)
+        @name = name
+    end 
+
+    def songs
+        Song.all
+    end
+
+    def add_song(song)
+        song.artist = self
+    end
+
+    def add_song_by_name(song)
+        song = Song.new(song)
+        song.artist = self
+    end
+
+    def self.song_count
+        Song.all.select { |song| song.artist}.count
+    end
+end
---
+++ lib/author.rb
@@ -0,0 +1,38 @@
+require 'pry'
+require_relative './post.rb'
+
+
+class Author
+    
+                     
+    attr_accessor :name, :posts
+
+
+
+    def initialize(name)
+        @name = name
+    end
+
+    def posts
+        Post.all
+    end
+
+    def add_post(post)
+        post.author = self
+    
+    end
+
+    def add_post_by_title(post_title)
+        post_title = Post.new(post_title)
+        post_title.author = self
+    end
+    
+    def self.post_count
+       Post.all.select { |post| post.author}.count
+        # if posts.count { |post| post.author == self}
+        #end
+    end
+end
+
+
+
